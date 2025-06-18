cdf8f6ddf913cf959e391fb7ed12e4e83db26fa9
Fathimath Nahla Salami E <nahlasalami321@gmail.com>
2025-06-18 22:39:59 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../css/home.css"
+
+
+const Home = () => {
+  return (
+    <div className='page my-details'>
+      <div className='my-paragraph div1'>
+        <h1>Fathimath Nahla Salami E</h1>
+        <h2>Software Developer</h2>
+      </div>
+      <div className='my-image div1'>
+        image
+      </div>
+    </div>
+  )
+}
+
+export default Home
